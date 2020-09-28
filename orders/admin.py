26478bd41277e3d85266e69f5dae8928dfd38a4a
Deme <dpk.williams@gmail.com>
2020-09-28 11:09:49 +0100

    added an action - csv download to the admin functonality

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,38 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.http import HttpResponse
+import csv
+import datetime
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)  # create a csv writer
+    fields = [field for field in opts.get_fields() if not \
+        field.many_to_many and not field.one_to_many]
+    # write the first row with header information
+    writer.writerow(field.verbose_name for field in fields)
+    # write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'  # display name
 
 
 @admin.register(Order)
@@ -14,5 +42,6 @@ class OrderAdmin(admin.ModelAdmin):
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
 
 # an inline allows you yo include a smodel on the same edit page as its relating model
