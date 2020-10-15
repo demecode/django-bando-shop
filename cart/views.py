4206cdbbd07e94773dc791e98eef31f3042bb684
Deme <dpk.williams@gmail.com>
2020-10-15 11:22:20 +0100

    product recommendaton aded to cart item views

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 from coupons.forms import CouponApplyForm
+from shop.recommender import Recommender
 
 
 # Create your views here.
@@ -37,8 +38,13 @@ def cart_detail(request):
         item['update_quantity_form'] = CartAddProductForm(initial={
             'quantity': item['quantity'],
             'override': True})
-        coupon_apply_form = CouponApplyForm
+        coupon_apply_form = CouponApplyForm()
+        r = Recommender()
+        cart_products = [item['product'] for item in cart]
+        print(cart_products)
+        recommended_products = r.suggest_products_for(cart_products, max_results=3)
         if coupon_apply_form is None:
             return render(request, 'cart/detail.html', {'cart': cart})
-    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
+    return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form,
+                                                'recommended_products': recommended_products})
     # the cart_detail gets the current cart to review
